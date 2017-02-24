1ed2baf63316fa03bc34e03ede9b61d31d4cd773
berkovyuriy <berkovyuriy@ukr.net>
2017-02-24 15:33:42 +0200

    Article Widgets: LastIssues and SeeAlso

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,6 +12,7 @@ class AppAsset extends AssetBundle {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+    //    'css/site.css', //
 	'/bootstrap/css/bootstrap.min.css',
 	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
 	'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
@@ -23,9 +24,10 @@ class AppAsset extends AssetBundle {
 	'/plugins/datepicker/datepicker3.css',
 	'/plugins/daterangepicker/daterangepicker.css',
 	'/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+
     ];
     public $js = [
-	'/plugins/jQuery/jquery-2.2.3.min.js',
+	//'/plugins/jQuery/jquery-2.2.3.min.js',
 	'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
 	'/bootstrap/js/bootstrap.min.js',
 	'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
@@ -41,12 +43,13 @@ class AppAsset extends AssetBundle {
 	'/plugins/slimScroll/jquery.slimscroll.min.js',
 	'/plugins/fastclick/fastclick.js',
 	'/dist/js/app.min.js',
-	'/dist/js/pages/dashboard.js',
+	//'/dist/js/pages/dashboard.js',
 	'/dist/js/demo.js',
+
     ];
     public $depends = [
-	    //'yii\web\YiiAsset',
-	    //'yii\bootstrap\BootstrapAsset',
+	//    'yii\web\YiiAsset',//
+	//    'yii\bootstrap\BootstrapAsset',//
     ];
 
 }
